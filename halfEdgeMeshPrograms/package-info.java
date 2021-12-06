042e63f0922ff3084d25066e7c1976165945a611
wenger.4@osu.edu <wenger.4@osu.edu>
2021-12-06 16:56:52 -0500

    Added package_info

---
+++ halfEdgeMeshPrograms/package-info.java
@@ -0,0 +1,12 @@
+/** Programs using the half edge mesh data structure.
+ *  <p>
+ *  <ul>
+ *  <li> meshinfo - Output mesh information such as number of vertices,
+ *         edges, cells, minimum and maximum edge lengths, and
+ *         minimum and maximum cell angles.
+ *  <li> decimate_mesh - Apply collapse/join/split edge or cell operations
+ *         to a mesh.
+ *  </ul>
+ */
+
+package halfEdgeMeshPrograms;
